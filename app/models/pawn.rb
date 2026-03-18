ce85b46dd0cf791c9730ebb6731c32d0cee1e810
Darren Cauthon <darren@cauthon.com>
2026-03-17 21:55:13 -0500

    The pawn is now circle.

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,5 +1,7 @@
 class Pawn
   PAWN_SIZE = 28
+  WHITE_PAWN_SPRITE = "sprites/pawn_white.png".freeze
+  BLACK_PAWN_SPRITE = "sprites/pawn_black.png".freeze
 
   attr_reader :col, :row, :color, :cell_width, :cell_height, :player
 
@@ -22,29 +24,26 @@ class Pawn
   end
 
   def render_at(args, pawn_x, pawn_y)
-    args.outputs.solids << {
+    args.outputs.sprites << {
       x: pawn_x,
       y: pawn_y,
       w: PAWN_SIZE,
       h: PAWN_SIZE,
-      r: color[0],
-      g: color[1],
-      b: color[2]
-    }
-
-    args.outputs.borders << {
-      x: pawn_x,
-      y: pawn_y,
-      w: PAWN_SIZE,
-      h: PAWN_SIZE,
-      r: 220,
-      g: 70,
-      b: 70
+      path: sprite_path
     }
   end
 
   def move_to(col, row)
     @col = col
     @row = row
+  end
+
+  private
+
+  def sprite_path
+    brightness = color[0] + color[1] + color[2]
+    return WHITE_PAWN_SPRITE if brightness >= 400
+
+    BLACK_PAWN_SPRITE
   end
 end
--- app/tests/pawn_test.rb
+++ app/tests/pawn_test.rb
@@ -5,36 +5,44 @@ def test_pawn_stores_grid_position(args, assert)
   assert.equal! 5, pawn.row, "Expected pawn row to be stored."
 end
 
-def test_pawn_render_outputs_one_solid_and_one_border(args, assert)
-  pawn = Pawn.new(2, 4, [100, 110, 120], player: PawnTestFakePlayer.new("Tester"), cell_width: 48, cell_height: 48)
+def test_pawn_render_outputs_one_sprite(args, assert)
+  pawn = Pawn.new(2, 4, [245, 245, 245], player: PawnTestFakePlayer.new("Tester"), cell_width: 48, cell_height: 48)
   fake_args = PawnTestFakeArgs.new
 
   pawn.render(fake_args, 100, 200, 6)
 
-  assert.equal! 1, fake_args.outputs.solids.length, "Expected one solid output."
-  assert.equal! 1, fake_args.outputs.borders.length, "Expected one border output."
+  assert.equal! 1, fake_args.outputs.sprites.length, "Expected one pawn sprite output."
 end
 
 def test_pawn_render_places_pawn_in_expected_cell(args, assert)
-  pawn = Pawn.new(2, 4, [100, 110, 120], player: PawnTestFakePlayer.new("Tester"), cell_width: 48, cell_height: 48)
+  pawn = Pawn.new(2, 4, [245, 245, 245], player: PawnTestFakePlayer.new("Tester"), cell_width: 48, cell_height: 48)
   fake_args = PawnTestFakeArgs.new
 
   pawn.render(fake_args, 100, 200, 6)
 
-  solid = fake_args.outputs.solids[0]
-  assert.equal! 218, solid[:x], "Expected pawn x to be centered in target cell."
-  assert.equal! 426, solid[:y], "Expected pawn y to be centered in target cell."
-  assert.equal! 28, solid[:w], "Expected pawn width to be 28."
-  assert.equal! 28, solid[:h], "Expected pawn height to be 28."
-  assert.equal! 100, solid[:r], "Expected pawn red color channel to match."
-  assert.equal! 110, solid[:g], "Expected pawn green color channel to match."
-  assert.equal! 120, solid[:b], "Expected pawn blue color channel to match."
+  sprite = fake_args.outputs.sprites[0]
+  assert.equal! 218, sprite[:x], "Expected pawn x to be centered in target cell."
+  assert.equal! 426, sprite[:y], "Expected pawn y to be centered in target cell."
+  assert.equal! 28, sprite[:w], "Expected pawn width to be 28."
+  assert.equal! 28, sprite[:h], "Expected pawn height to be 28."
+  assert.equal! "sprites/pawn_white.png", sprite[:path], "Expected light pawn to use white pawn sprite."
+end
+
+def test_pawn_render_uses_black_sprite_for_dark_pawn(args, assert)
+  pawn = Pawn.new(2, 4, [50, 50, 50], player: PawnTestFakePlayer.new("Tester"), cell_width: 48, cell_height: 48)
+  fake_args = PawnTestFakeArgs.new
+
+  pawn.render(fake_args, 100, 200, 6)
+
+  sprite = fake_args.outputs.sprites[0]
+  assert.equal! "sprites/pawn_black.png", sprite[:path], "Expected dark pawn to use black pawn sprite."
 end
 
 class PawnTestFakeOutputs
-  attr_accessor :solids, :borders
+  attr_accessor :sprites, :solids, :borders
 
   def initialize
+    @sprites = []
     @solids = []
     @borders = []
   end
