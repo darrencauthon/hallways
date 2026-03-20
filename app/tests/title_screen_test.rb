a5e021cbfe1b2e195591915820f7d7a19b3080e4
Darren Cauthon <darren@cauthon.com>
2026-03-19 20:04:02 -0500

    Bump th eversion.

--- app/tests/title_screen_test.rb
+++ app/tests/title_screen_test.rb
@@ -37,8 +37,8 @@ def test_title_screen_renders_version_label(args, assert)
   fake_args = TitleScreenTestHelpers.build_fake_args
   screen.tick(fake_args)
 
-  version_label = fake_args.outputs.labels.find { |label| label[:text] == "v0.2.1" }
-  assert.equal! false, version_label.nil?, "Expected title screen to render version label v0.2.1."
+  version_label = fake_args.outputs.labels.find { |label| label[:text] == "v0.3" }
+  assert.equal! false, version_label.nil?, "Expected title screen to render version label v0.3."
 end
 
 def test_title_screen_renders_continue_game_option_when_available(args, assert)
